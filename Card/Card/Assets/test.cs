fd45c910193f1aebd2be7921e4680e7c1f5b317e
Yinhang <yinhang_joy@sina.com>
2017-10-10 17:48:30 +0800

    no message

---
+++ Card/Card/Assets/test.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public interface test  {
+    string s { get; set; }
+    void show();
+}
+public class test1
+{
+    public void show(test test)
+    {
+        test.show();
+    }
+}
